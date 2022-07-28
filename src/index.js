0b81ae1966c9c9580da014266989347fe64a442c
Aaron Fabricio <aaronfabricio00@gmail.com>
2022-07-28 17:45:18 -0400

    primer

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { dirname } from "path";
+
+import { fileURLToPath } from "url";
+const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+console.log(__dirname);
+
+app.set("views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000);
+
+console.log("listenig ", 3000);
